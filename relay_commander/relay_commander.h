817e513cf92e88967c835f5546bc9d05c5637ef8
Keith Lee <keith.lee@gumstix.com>
2018-09-21 08:12:30 -0700

    Relay commander early draft.

---
+++ relay_commander/relay_commander.h
@@ -0,0 +1,71 @@
+#ifndef __RELAY_COMMANDER_H__
+#define __RELAY_COMMANDER_H__
+
+#define MAX_WIFI_ATTEMPTS 5
+#define DEBUG
+
+const int server_port = 80;
+
+const int num_relays = 10;
+
+const unsigned long debounce_delay = 50;
+
+typedef struct relay_state_t {
+  int current;
+  int next; 
+} relay_state;
+
+typedef struct wpa_data_t {
+  char ssid[33];
+  char psk[64];
+} wpa_data;
+
+typedef struct debounce_state_t {
+  int button_state;
+  int last_button_state;
+  unsigned long last_debounce_time;
+  int value;
+} debounce_state;
+
+typedef enum lockout_value_t {none, console, network} lockout_value;
+
+typedef enum serial_cmd_t { no_cmd, 
+                            set_wifi_ssid = 'w', 
+                            set_wifi_psk = 'p', 
+                            connect_wifi = 'c', 
+                            disconnect_wifi = 'd', 
+                            enable_relay = 'e', 
+                            disable_relay = 'd',
+                            lockout_serial = 'l'
+                          } serial_cmd;
+
+
+
+char valid_cmds[] = { 
+                      'w', // Set WiFi SSID
+                      'p', // Set WiFi PSK
+                      'c', // Attempt to connect to WiFi
+                      'l', // Lockout
+                      't', // Toggle relay
+                      'a', // Activate relay
+                      's'  // Deactivate relay
+                    };
+
+
+char cmd[32];
+
+
+int cmd_get_relay() {
+  int relay = num_relays + 1;
+  int i = 1;
+  while(relay > num_relays && i <=32) {
+    if(cmd[i] >= '0' && cmd[i] < ('0' + num_relays)) {
+            relay = atoi(&(cmd[i]));
+          }
+    else if(cmd[i] == 0 || cmd[i] =='\n')
+      break;
+  }
+  return relay;
+}
+
+#endif
--- relay_commander/relay_commander.ino
+++ relay_commander/relay_commander.ino
@@ -6,40 +6,7 @@
 
 #include <SPI.h>
 #include <WiFi101.h>
-
-const int num_relays = 10;
-
-typedef enum lockout_value_t {none, console, network} lockout_value;
-
-lockout_value lockout;
-
-typedef struct relay_state_t {
-  int current;
-  int next; 
-} relay_state;
-
-typedef struct wpa_data_t {
-  char ssid[100];
-  char psk[100];
-} wpa_data;
-
-typedef struct debounce_state_t {
-  int button_state;
-  int last_button_state;
-  unsigned long last_debounce_time;
-  int value;
-} debounce_state;
-
-const unsigned long debounce_delay = 50;
-
-
-relay_state relay_states[num_relays];
-debounce_state buttons[num_relays];
-
-wpa_data credentials;
-int wifi_status = WL_IDLE_STATUS;
-
-FlashStorage(wpa_credentials, wpa_data);
+#include "relay_commander.h"
 
 int relay_clockwise[] =   {
                             PB3,
@@ -66,53 +33,50 @@ int button_clockwise[] =  {
                             PB2
                           };
 
+FlashStorage(wpa_credentials, wpa_data);
+
+lockout_value lockout;
+
+relay_state relay_states[num_relays];
+debounce_state buttons[num_relays];
+
+wpa_data credentials;
+int wifi_status = WL_IDLE_STATUS;
+
 int seq_index;
 
-void attempt_login() {
-  int attempts = 0;
-  while(wifi_status != WL_CONNECTED && attempts < MAX_WIFI_ATTEMPTS) {
-    wifi_status = WiFi.begin(credentials.ssid, credentials.psk);
-    delay(5000);
-  }
-  #ifdef DEBUG
-    if(wifi_status == WL_CONNECTED){
-      Serial.println("---------------\nWiFi Connected\n---------------");
-    }
-    else {
-      Serial.println("---------------\nWiFi Could Not Connect\n---------------");
-    }
-  #endif
-}
+WiFiServer server(server_port);
 
-void get_debounced(int i) {
-  buttons[i].button_state = digitalRead(button_clockwise[i]);
-  if(buttons[i].button_state != buttons[i].last_button_state) {
-    buttons[i].last_debounce_time = millis();
-  }
-  if((millis() - buttons[i].last_debounce_time) > debounce_delay && buttons[i].button_state != buttons[i].value) {
-    buttons[i].value = buttons[i].button_state;
-    buttons[i].last_debounce_time = 0;
-    #ifdef DEBUG
-      Serial.print("---------------\nDebounce Relay Button:\n");
-      Serial.print(i);
-      Serial.print(": ");
-      Serial.print(buttons[i].value);
-      Serial.print("\n---------------\n");
-    #endif
-  }
-}
+WiFiClient client;
+////////////////////////////////////////////////////////////////////
+//                     RELAY CONTROL SYSTEM                       //
+////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////
+// SET_NEXT_STATE
+// Setup value to be written to each relay
 
 void set_next_state(int i, int toggle) {
   if(toggle == HIGH) {
     relay_states[i].next = !relay_states[i].next;
   }
 }
+//////////////////////////////////////////////////////////////////
+// GET_ALL_RELAYS
+// Get actual pin values for each relay to ensure the correct
+// relays are modified when in set cycle
 
 void get_all_relays() {
   for(int i = 0; i < num_relays; i++) {
     relay_states[i].current = digitalRead(relay_clockwise[i]);
   }
 }
+
+
+/////////////////////////////////////////////////////////////////
+// SET_ALL_RELAYS
+// Toggle relays set to change state
 
 void set_all_relays() {
   for(int i = 0; i < num_relays; i++) {
@@ -129,6 +93,189 @@ void set_all_relays() {
   }
 }
 
+////////////////////////////////////////////////////////////////////
+//                  PUSHBUTTON COMMAND SYSTEM                     //
+////////////////////////////////////////////////////////////////////
+
+
+
+//////////////////////////////////////////////////////////////////////
+// GET_DEBOUNCED
+// Store stabilized value for relay pushbuttons.
+
+void get_debounced(int i) {
+  buttons[i].button_state = digitalRead(button_clockwise[i]);
+  if(buttons[i].button_state != buttons[i].last_button_state) {
+    buttons[i].last_debounce_time = millis();
+  }
+  if((millis() - buttons[i].last_debounce_time) > debounce_delay 
+                  && buttons[i].button_state != buttons[i].value) {
+    buttons[i].value = buttons[i].button_state;
+    buttons[i].last_debounce_time = 0;
+    #ifdef DEBUG
+      Serial.print("---------------\nDebounce Relay Button:\n");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.print(buttons[i].value);
+      Serial.print("\n---------------\n");
+    #endif
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////
+//                    CONSOLE COMMAND SYSTEM                      //
+////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////
+// IS_VALID_CMD
+// Verify command
+
+bool is_valid_cmd(char cmd) {
+  for(int i = 0; i < sizeof(valid_cmds); i++) {
+    if(cmd == valid_cmds[i])
+      return true;
+  }
+  return false;
+}
+
+
+/////////////////////////////////////////////////
+// CONSOLE_GET_COMMAND
+// Collect any valid input from serial console
+
+void console_get_command() {
+  cmd[0] = 0;
+  if(Serial.available()) {
+    for(int i=0; Serial.available() && i < 32; i++) {
+      cmd[i] = Serial.read();
+      if(cmd[i] == '\n') break;
+    }
+    if(!is_valid_cmd(cmd[0]))
+      cmd[0] = 0;
+  }
+  return;
+}
+
+/////////////////////////////////////////////////
+// CONSOLE_PROCESS_COMMAND
+// take validated command and commit it
+//
+
+void console_process_command() {
+  int relay = num_relays + 1;        
+  int len = 0;
+  if (cmd[0] != 0) {
+    char key = cmd[0];
+    switch(key) {
+      case 'w': // Set wifi SSID
+         Serial.print("Please enter WiFi SSID:   ");
+         len = Serial.readBytesUntil('\n', credentials.ssid, 33);
+         credentials.ssid[len] = 0;
+         #ifdef DEBUG
+          Serial.print("WPA SSID = ");
+          Serial.println(credentials.ssid);
+        #endif
+         Serial.println("\nData will not be saved to FLASH until (c)onnect!"); 
+         break;
+      case 'p': // Set wifi PSK
+        Serial.print("Please enter WiFi PSK:   ");
+        len = Serial.readBytesUntil('\n', credentials.psk, 64);
+        credentials.psk[len] = 0;
+        #ifdef DEBUG
+          Serial.print("WPA PSK = ");
+          Serial.println(credentials.psk);
+        #endif
+        Serial.println("\nData will not be saved to FLASH until (c)onnect!");
+        break;
+      case 'c': // Connect to wifi and save credentials if successful
+        Serial.print("Attempting to login to WiFi Network: ");
+        Serial.println(credentials.ssid);
+        if(attempt_login()) {
+          wpa_credentials.write(credentials);
+          Serial.println("Login successful!  New credentials saved!");
+        }
+        else
+          Serial.println("Login failed!");
+        break;
+      case 'l': //Lockout pushbuttons and WiFi
+        if(lockout == none) {
+          lockout = console;
+          Serial.println("Console Lockout Enabled!");
+        }
+        else if(lockout == console) {
+          lockout = none;
+          Serial.println("Console Lockout Disabled!");
+        }
+        break;
+      case 't': //Toggle relay
+        relay = cmd_get_relay();
+        if(relay < num_relays) {
+          if(relay_states[relay].current == HIGH)  
+            relay_states[relay].next = LOW;
+          else
+            relay_states[relay].next = HIGH;
+        }
+      case 'd':
+      default:
+      break;
+    }
+  }
+}
+
+void console_put_response() {
+  return;
+}
+
+
+////////////////////////////////////////////////////////////////////
+//                    NETWORK COMMAND SYSTEM                      //
+////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////
+// ATTEMPT_LOGIN:
+// Using credentials acquired from the FLASH memory, attempt to connect
+// to WIFI via WPA.
+
+bool attempt_login() {
+  int attempts = 0;
+  while(wifi_status != WL_CONNECTED && attempts < MAX_WIFI_ATTEMPTS) {
+    wifi_status = WiFi.begin(credentials.ssid, credentials.psk);
+    delay(5000);
+  }
+  #ifdef DEBUG
+    if(wifi_status == WL_CONNECTED){
+      Serial.println("---------------\nWiFi Connected\n---------------");
+      server.begin();
+    }
+    else {
+      Serial.println("---------------\nWiFi Could Not Connect\n---------------");
+    }
+  #endif
+  return(wifi_status == WL_CONNECTED);
+}
+
+
+  void tcp_get_command() {
+    if(!client) 
+      client = server.available();
+    if(client) {
+      while(client.connected()) {
+        if client.available() {
+          
+        }
+      }
+    }
+    return;  
+  }
+
+  void tcp_process_command() {
+    return;
+  }
+
+  void tcp_put_response() {
+    return;
+  }
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -149,28 +296,29 @@ void setup() {
   lockout = none;
   seq_index = 0;
   
-  credentials = wpa_credentials.read()
+  credentials = wpa_credentials.read();
 
   attempt_login();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
   if(lockout == none) {
     get_debounced(seq_index);
     set_next_state(seq_index, buttons[seq_index].value);
   }
   
   if(lockout != network) {
-    cmd = console_get_command();
-    result = process_command(cmd);
-    console_put_response(result);
+    console_get_command();
+    console_process_command();
+    console_put_response();
   }
   
   if(lockout != console) {
-    cmd = tcp_get_command();
-    result = process_command(cmd);
-    tcp_put_response(result);
+    tcp_get_command();
+    tcp_process_command();
+    tcp_put_response();
   }
   
   get_all_relays();
